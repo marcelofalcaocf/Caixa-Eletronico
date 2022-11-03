c17889c33f88191f2bd90114fe8a9f1285921d3e
Marcelo Falcão <marcelo_falcaoc@hotmail.com>
2022-11-03 14:54:37 -0300

    desafio 1 o caixa eletronico

---
+++ Caixa Eletronico.playground/Contents.swift
@@ -0,0 +1,55 @@
+import UIKit
+
+func cashMachine(value: Double) -> String {
+    let availableValues: [Double] = [50.0,10.0,5.0,2.0]
+    
+    var fiftyCount = 0
+    var tenCount = 0
+    var fiveCount = 0
+    var twoCount = 0
+    var remainingAmount = value
+    var result: String = ""
+    
+    
+    while remainingAmount > 0 {
+        if Int(remainingAmount/availableValues[0]) > 0 {
+            fiftyCount = Int(remainingAmount/availableValues[0])
+            remainingAmount -= Double(fiftyCount) * availableValues[0]
+            result += "\(fiftyCount) nota(s) de 50"
+        }
+        if Int(remainingAmount/availableValues[1]) > 0 {
+            tenCount = Int(remainingAmount/availableValues[1])
+            remainingAmount -= Double(tenCount) * availableValues[1]
+            if fiftyCount > 0 {
+                result += ", \(tenCount) nota(s) de 10"
+            } else {
+                result += "\(tenCount) nota(s) de 10"
+            }
+        }
+        if Int(remainingAmount/availableValues[2]) > 0 {
+            fiveCount = Int(remainingAmount/availableValues[2])
+            remainingAmount -= Double(fiveCount) * availableValues[2]
+            if fiftyCount > 0 || tenCount > 0 {
+                result += ", \(fiveCount) nota(s) de 5"
+            } else {
+                result += "\(fiveCount) nota(s) de 5"
+            }
+        }
+        if Int(remainingAmount/availableValues[3]) > 0 {
+            twoCount = Int(remainingAmount/availableValues[3])
+            remainingAmount -= Double(twoCount) * availableValues[3]
+            if fiveCount > 0 || tenCount > 0 || twoCount > 0 {
+                result += ", \(twoCount) nota(s) de 2"
+            } else {
+                result += "\(twoCount) nota(s) de 2"
+            }
+        }
+    }
+    
+    return result
+}
+
+print(cashMachine(value: 600))
+
+
+
