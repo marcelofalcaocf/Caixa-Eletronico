1920cb284da5d64784db1d1d9c8f30c10233b5cb
Marcelo Falcão <marcelo_falcaoc@hotmail.com>
2022-11-04 11:52:20 -0300

    ajuste de erro quando sobrar dinheiro

--- Caixa Eletronico.playground/Contents.swift
+++ Caixa Eletronico.playground/Contents.swift
@@ -1,5 +1,12 @@
 import UIKit
 
+func validaValue(value: Double, money: Int ) -> Bool {
+    if Int(value) > money {
+        return true
+    }
+    return false
+}
+
 func cashMachine(value: Double) -> String {
     let availableValues: [Double] = [50.0,10.0,5.0,2.0]
     
@@ -11,21 +18,22 @@ func cashMachine(value: Double) -> String {
     var result: String = ""
     
     
-    while remainingAmount > 0 {
         if Int(remainingAmount/availableValues[0]) > 0 {
             fiftyCount = Int(remainingAmount/availableValues[0])
             remainingAmount -= Double(fiftyCount) * availableValues[0]
             result += "\(fiftyCount) nota(s) de 50"
         }
+        
         if Int(remainingAmount/availableValues[1]) > 0 {
             tenCount = Int(remainingAmount/availableValues[1])
-            remainingAmount -= Double(tenCount) * availableValues[1]
-            if fiftyCount > 0 {
-                result += ", \(tenCount) nota(s) de 10"
-            } else {
-                result += "\(tenCount) nota(s) de 10"
-            }
+                remainingAmount -= Double(tenCount) * availableValues[1]
+                if fiftyCount > 0 {
+                    result += ", \(tenCount) nota(s) de 10"
+                } else {
+                    result += "\(tenCount) nota(s) de 10"
+                }
         }
+        
         if Int(remainingAmount/availableValues[2]) > 0 {
             fiveCount = Int(remainingAmount/availableValues[2])
             remainingAmount -= Double(fiveCount) * availableValues[2]
@@ -35,6 +43,7 @@ func cashMachine(value: Double) -> String {
                 result += "\(fiveCount) nota(s) de 5"
             }
         }
+        
         if Int(remainingAmount/availableValues[3]) > 0 {
             twoCount = Int(remainingAmount/availableValues[3])
             remainingAmount -= Double(twoCount) * availableValues[3]
@@ -44,12 +53,14 @@ func cashMachine(value: Double) -> String {
                 result += "\(twoCount) nota(s) de 2"
             }
         }
-    }
+        
+        if Int(remainingAmount - value) != 0 {
+            result = "Por gentileza, informa valores compativeis com a maquina"
+        }
     
     return result
 }
 
-print(cashMachine(value: 600))
-
+print(cashMachine(value: 8))
 
 
