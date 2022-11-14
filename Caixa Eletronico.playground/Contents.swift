9b94d4e7416ff2b011a9069b94a2b65ff6b42058
Marcelo Falcão <marcelo_falcaoc@hotmail.com>
2022-11-14 19:46:42 -0300

    atualizei o codigo e desafio um e dois estão funcionando

--- Caixa Eletronico.playground/Contents.swift
+++ Caixa Eletronico.playground/Contents.swift
@@ -1,78 +1,168 @@
 import UIKit
 
-func validaValue(value: Double, money: Int ) -> Bool {
-    if Int(value) > money {
-        return true
+//// MARK: DevPoli
+//
+//let notasDisponiveis = [50,10, 5, 2]
+//
+//
+//func sacar(retirar: Int) -> Bool{
+//    guard retirar > 0 else {
+//        return true //NAO TENHO SALDO REMANESCENTE
+//    }
+//    for nota in notasDisponiveis {
+//        if retirar >= nota {
+//            let novoSaque = retirar - nota
+//            if sacar(retirar: novoSaque) {
+//                print("Moeda: \(nota)")
+//                return true
+//            }
+//        }
+//    }
+//    return false //NAO TENHO NOTA DISPONIVEL
+//}
+//
+//// sacar(retirar: 28)
+//
+//
+//// MARK: Marcelo
+//
+//let availableNotes = [50,10, 5, 2]
+//
+//func withdrawMoney(retirar: Int) -> String? {
+//    var result: String = ""
+//
+//    guard retirar > 0 else {
+//        return  result //NAO TENHO SALDO REMANESCENTE
+//    }
+//
+//    for nota in availableNotes {
+//
+//        if retirar >= nota {
+//            let novoSaque = retirar - nota
+//            if (withdrawMoney(retirar: novoSaque) != nil) {
+//                if let resultado  = withdrawMoney(retirar: novoSaque) {
+//                    if resultado != "" {
+//                        result += "Moeda \(nota) + \(resultado)"
+//                        print(result)
+//                        return result
+//                    }
+//                }
+//                result += "Moeda \(nota)"
+//                print(result)
+//                return result
+//            }
+//        }
+//    }
+//    return nil
+//}
+
+// withdrawMoney(retirar: 8)
+
+
+
+// MARK: Marcelo com dic
+
+//let notes = [50,10, 5, 2]
+//
+//func withdraw(retirar: Int) -> [Int: Int]? {
+//    var resultsNotes: [Int: Int] = [:]
+//
+//    guard retirar > 0 else {
+//        return  resultsNotes //NAO TENHO SALDO REMANESCENTE
+//    }
+//
+//    for nota in notes {
+//        if retirar >= nota {
+//            let novoSaque = retirar - nota
+//            if (withdraw(retirar: novoSaque) != nil)  {
+//                if let resultado = withdraw(retirar: novoSaque) {
+//                    if resultado != [:] {
+//                        resultsNotes = [nota : 1]
+//                        let valueDic = resultsNotes[nota] ?? 0
+//
+//                        return resultsNotes
+//                    }
+//                }
+//                resultsNotes.updateValue(1, forKey: nota)
+//                print(resultsNotes)
+//                return resultsNotes
+//            }
+//        }
+//    }
+//    return nil
+//}
+//
+//withdraw(retirar: 8)
+//
+//// MARK: Exercício 1 pronto
+//
+//let notes = [50,10, 5, 2]
+//
+//func withdraw(retirar: Int) -> [Int: Int]? {
+//    var resultsNotes: [Int: Int] = [:]
+//
+//    guard retirar > 0 else {
+//        return  resultsNotes //NAO TENHO SALDO REMANESCENTE
+//    }
+//
+//    for nota in notes {
+//        if retirar >= nota {
+//            let novoSaque = retirar - nota
+//            if var result = withdraw(retirar: novoSaque) {
+//                var valor = result[nota] ?? 0
+//                valor += 1
+//                result.updateValue(valor, forKey: nota)
+//                print(result)
+//                return result
+//            }
+//        }
+//    }
+//    return nil
+//}
+//
+//withdraw(retirar: 8)
+
+// MARK: Exercício 2
+
+struct Note {
+    var note: Int
+    var inventory: Int
+}
+
+var notesAvailable: [Note] = [
+    Note(note: 50, inventory: 2),
+    Note(note: 10, inventory: 5),
+    Note(note: 5, inventory: 5),
+    Note(note: 2, inventory: 5
+        )
+]
+
+var notesWithIndex = notesAvailable.enumerated()
+
+func withdraw(retirar: Int) -> [Int: Int]? {
+    var resultsNotes: [Int: Int] = [:]
+    
+    guard retirar > 0 else {
+        return  resultsNotes //NAO TENHO SALDO REMANESCENTE
     }
-    return false
+    
+    for (index, note) in notesWithIndex {
+        if retirar >= note.note && notesAvailable[index].inventory > 0 {
+            var newInvetory = notesAvailable[index].inventory
+            newInvetory -= 1
+            notesAvailable[index].inventory =  newInvetory
+            
+            let newWithdraw = retirar - note.note
+            if var result = withdraw(retirar: newWithdraw) {
+                var newValue = result[note.note] ?? 0
+                newValue += 1
+                result.updateValue(newValue, forKey: note.note)
+                return result
+            }
+        }
+    }
+    return nil
 }
 
-func cashMachine(value: Double) -> String {
-    let availableValues: [Double] = [50.0,10.0,5.0,2.0]
-    
-    var fiftyCount = 0
-    var tenCount = 0
-    var fiveCount = 0
-    var twoCount = 0
-    var remainingAmount = value
-    var result: String = ""
-    
-    
-        if Int(remainingAmount/availableValues[0]) > 0 {
-            fiftyCount = Int(remainingAmount/availableValues[0])
-            remainingAmount -= Double(fiftyCount) * availableValues[0]
-            result += "\(fiftyCount) nota(s) de 50"
-        }
-        
-        if Int(remainingAmount/availableValues[1]) > 0 {
-            tenCount = Int(remainingAmount/availableValues[1])
-                remainingAmount -= Double(tenCount) * availableValues[1]
-                if fiftyCount > 0 {
-                    result += ", \(tenCount) nota(s) de 10"
-                } else {
-                    result += "\(tenCount) nota(s) de 10"
-                }
-        }
-        
-        if Int(remainingAmount/availableValues[2]) > 0 {
-            fiveCount = Int(remainingAmount/availableValues[2])
-            remainingAmount -= Double(fiveCount) * availableValues[2]
-            if fiftyCount > 0 || tenCount > 0 {
-                result += ", \(fiveCount) nota(s) de 5"
-            } else {
-                result += "\(fiveCount) nota(s) de 5"
-            }
-        }
-        
-        if Int(remainingAmount/availableValues[3]) > 0 {
-            twoCount = Int(remainingAmount/availableValues[3])
-            remainingAmount -= Double(twoCount) * availableValues[3]
-            
-            if remainingAmount != 0 {
-                remainingAmount = value
-                fiftyCount = 0
-                tenCount = 0
-                fiveCount = 0
-                twoCount = 0
-                
-                twoCount = Int(remainingAmount/availableValues[3])
-                remainingAmount -= Double(twoCount) * availableValues[3]
-            }
-            
-            if fiveCount > 0 || tenCount > 0 || twoCount > 0 {
-                result += ", \(twoCount) nota(s) de 2"
-            } else {
-                result += "\(twoCount) nota(s) de 2"
-            }
-        }
-        
-        if Int(remainingAmount) != 0 {
-            result = "Por gentileza, informa valores compativeis com a maquina"
-        }
-    
-    return result
-}
-
-print(cashMachine(value: 18))
-
+withdraw(retirar: 150)
 
